c8281c24abdd3c91d048b51fa5ac9fd6b9307d2b
athlonreg <15563836030@163.com>
2018-08-09 22:25:16 +0800

    自定义分辨率实现(Credits 宪武)

--- hidpi.sh
+++ hidpi.sh
@@ -3,9 +3,15 @@
 function init()
 {
 cat << EEF
-----------------------------------------
-|*************** HIDPI ****************|
-----------------------------------------
+============================================
+  _    _   _____   _____    _____    _____ 
+ | |  | | |_   _| |  __ \  |  __ \  |_   _|
+ | |__| |   | |   | |  | | | |__) |   | |  
+ |  __  |   | |   | |  | | |  ___/    | |  
+ | |  | |  _| |_  | |__| | | |       _| |_ 
+ |_|  |_| |_____| |_____/  |_|      |_____|
+                                           
+============================================
 EEF
     VendorID=$(ioreg -l | grep "DisplayVendorID" | awk '{print $8}')
     ProductID=$(ioreg -l | grep "DisplayProductID" | awk '{print $8}')
@@ -14,22 +20,15 @@ EEF
     Vid=$(echo "obase=16;$VendorID" | bc | tr 'A-Z' 'a-z')
     Pid=$(echo "obase=16;$ProductID" | bc | tr 'A-Z' 'a-z')
 
-    edID=$(echo $EDID | sed 's/../b5/21')
-
-    EDid=$(echo $edID | xxd -r -p | base64)
     thisDir=$(dirname $0)
     thatDir="/System/Library/Displays/Contents/Resources/Overrides"
 
     Overrides="\/System\/Library\/Displays\/Contents\/Resources\/Overrides\/"
 
     DICON="com\.apple\.cinema-display"
-
     imacicon=${Overrides}"DisplayVendorID-610\/DisplayProductID-a032.tiff"
-
     mbpicon=${Overrides}"DisplayVendorID-610\/DisplayProductID-a030-e1e1df.tiff"
-
     mbicon=${Overrides}"DisplayVendorID-610\/DisplayProductID-a028-9d9da0.tiff"
-
     lgicon=${Overrides}"DisplayVendorID-1e6d\/DisplayProductID-5b11.tiff"
 
     if [[ ! -d $thatDir/backup ]]; then
@@ -49,7 +48,7 @@ function choose_icon()
 
 cat << EOF
 ----------------------------------------
-|********** 选择要显示的ICON ***********|
+|**********   选择显示器图标   ***********|
 ----------------------------------------
 (1) iMac
 (2) MacBook
@@ -75,7 +74,7 @@ DICON=${Overrides}"DisplayVendorID-1e6d\/DisplayProductID-5b11.icns"
 ;;
 5) rm -rf $thisDir/tmp/Icons.plist
 ;;
-*) echo "输入错误，拜拜";
+*) echo "输入错误，程序退出";
 exit 0
 ;;
 esac 
@@ -89,6 +88,15 @@ if [[ $Picon ]]; then
     sed -i '' "s/RPH/${RP[3]}/g" $thisDir/tmp/Icons.plist
     sed -i '' "s/PICON/$Picon/g" $thisDir/tmp/Icons.plist
     sed -i '' "s/DICON/$DICON/g" $thisDir/tmp/Icons.plist
+
+#    gsed -i "s/VID/$Vid/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/PID/$Pid/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/RPX/${RP[0]}/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/RPY/${RP[1]}/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/RPW/${RP[2]}/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/RPH/${RP[3]}/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/PICON/$Picon/g" $thisDir/tmp/Icons.plist
+#    gsed -i "s/DICON/$DICON/g" $thisDir/tmp/Icons.plist
 fi
 
 }
@@ -109,7 +117,7 @@ cat > "$dpiFile" <<-\HIDPI
         <key>DisplayBluePointX</key>
         <real>0.155</real>
         <key>DisplayBluePointY</key>
-        <real>0.07</real>
+        <real>0.06</real>
         <key>DisplayGreenGamma</key>
         <real>2</real>
         <key>DisplayGreenPointX</key>
@@ -136,27 +144,34 @@ cat > "$dpiFile" <<-\HIDPI
         <data>EDid</data>
         <key>scale-resolutions</key>
             <array>
-                <data>
-                AAAPAAAACHAA
-                </data>
-                <data>
-                AAAMgAAABkAA
-                </data>
-                <data>
-                AAAMgAAABwgA
-                </data>
-                <data>
-                AAALQAAABlQA
-                </data>
+HIDPI
+
+read -p "是否自定义分辨率 (y/n) : " res
+case $res in
+
+y) custom_res
+;;
+n) not_custom_res
+;;
+*) sudo rm -rf $thisDir/tmp
+   echo "输入无效，程序退出";
+   exit 0
+;;
+esac
+#
+cat >> "$dpiFile" <<-\FFF
             </array>
         <key>target-default-ppmm</key>
             <real>10.1510574</real>
     </dict>
 </plist>
-HIDPI
+FFF
 
     sed -i '' "s/VID/$VendorID/g" $dpiFile
     sed -i '' "s/PID/$ProductID/g" $dpiFile
+    
+#    gsed -i "s/VID/$VendorID/g" $dpiFile
+#    gsed -i "s/PID/$ProductID/g" $dpiFile
 }
 
 function end()
@@ -173,6 +188,9 @@ function enable_hidpi()
     main
     sed -i "" "/.*IODisplayEDID/d" $dpiFile
     sed -i "" "/.*EDid/d" $dpiFile
+    
+#    gsed -i "/.*IODisplayEDID/d" $dpiFile
+#    gsed -i "/.*EDid/d" $dpiFile
     end
 }
 
@@ -183,6 +201,62 @@ function disable()
     sudo cp -r $thatDir/backup/* $thatDir/
     sudo rm -rf $thatDir/backup
     echo "已关闭，重启生效"
+}
+
+#自定义分辨率
+function custom_res()
+{
+    echo "输入要开启的分辨率，以空格分隔，例如：1366x768 1440x810 1600x900 1920x1080"
+    read -p res
+    create_res $res
+}
+
+# 不自定义分辨率
+function not_custom_res()
+{
+    cat >> "$dpiFile" <<-\NCR
+                <data>
+                AAAPAAAACHAA
+                </data>
+                <data>
+                AAAMgAAABkAA
+                </data>
+                <data>
+                AAAMgAAABwgA
+                </data>
+                <data>
+                AAALQAAABlQA
+                </data>
+NCR
+}
+
+# 创建分辨率配置
+function create_res()
+{
+#注入xxxxXyyy00
+    for res in $@; do
+    width=$(echo $res | cut -d x -f 1)
+    height=$(echo $res | cut -d x -f 2)
+    hidpi=$(printf '%08x %08x' $(($width)) $(($height)) | xxd -r -p | base64)
+
+cat << OOO >> $dpiFile
+                <data>${hidpi:0:11}A</data>
+OOO
+done
+#注入2xxxxX2yyy00000001
+#注入2xxxxX2yyy00000001 00200000
+#注入2xxxxX2yyy00000009 00A00000
+    for res in $@; do
+    width=$(echo $res | cut -d x -f 1)
+    height=$(echo $res | cut -d x -f 2)
+    hidpi=$(printf '%08x %08x' $(($width*2)) $(($height*2)) | xxd -r -p | base64)
+#
+cat << OOO >> $dpiFile
+                <data>${hidpi:0:11}AAAAB</data>
+                <data>${hidpi:0:11}AAAABACAAAA==</data>
+                <data>${hidpi:0:11}AAAAJAKAAAA==</data>
+OOO
+done
 }
 
 function start()
